e598a8a45a3cb700470e65a55586375af1ce76ad
unknown <33568@ETECAF.LOCAL>
2019-04-05 17:44:21 -0300

    ArrayOfArray

---
+++ Assets/scripts/scrArray.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrArray : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var arr = array(2);
+        var mat = Matrix(2);
+        Debug.Log("" + arr[0][0]);
+        Debug.Log("" + mat[1, 2]);
+	}
+
+    int[][] array(int n)
+    {
+        int[][] arr = new int[3][];
+
+        arr[0] = new int[] { n };
+        arr[1] = new int[] { n + 1, n + 2 };
+        arr[2] = new int[] { n + 3, n + 4, n + 5};
+        return arr;
+    }
+
+    int[,] Matrix(int n)
+    {
+        int[,] mat = new int[3, 3] {    
+        {n, 0, 0},
+        {n + 1, n + 2, 0},
+        {n + 3, n + 4, n + 5},
+        };
+        return mat;
+    }
+}
